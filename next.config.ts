f3a4976b271b1c79ac3bf94457717ce40d1c9a94
William Chung <44889162+Krinkrinnnnn@users.noreply.github.com>
2026-06-08 10:59:27 +0800

    feat: configure next.js static export and github actions deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.GITHUB_REPOSITORY && !process.env.GITHUB_REPOSITORY.endsWith('.github.io')
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+    : '',
 };
 
 export default nextConfig;
